3580381fe7fa1a93929526eea3a0829b4668c215
UrbanWill <47801291+UrbanWill@users.noreply.github.com>
2019-12-31 14:58:20 +0900

    Added styling with bootstrap

--- src/components/GuessedWords.js
+++ src/components/GuessedWords.js
@@ -5,20 +5,22 @@ const GuessedWords = props => {
   let contents;
 
   if (props.guessedWords.length === 0) {
-    contents = <span data-test="guess-instructions">Try to guess!</span>;
+    contents = (
+      <span data-test="guess-instructions">Try to guess the secret word!</span>
+    );
   } else {
     const guessedWordRows = props.guessedWords.map((word, index) => (
       <tr data-test="guessed-word" key={index}>
-        <td>word.guessedWord</td>
-        <td>word.letterMatchCount</td>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
       </tr>
     ));
 
     contents = (
       <div data-test="guessed-words">
         <h3>Guessed Words</h3>
-        <table>
-          <thead>
+        <table className="table table-sm">
+          <thead className="thead-light">
             <tr>
               <th>Guess</th>
               <th>Matching Letters</th>
