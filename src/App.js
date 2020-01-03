ebb1e9b9d39702162a50f8f7cf07e225b92cddeb
UrbanWill <47801291+UrbanWill@users.noreply.github.com>
2020-01-03 16:53:49 +0900

    App fectches secretWord
    App fectches secretWord and update state

--- src/App.js
+++ src/App.js
@@ -2,8 +2,33 @@ import React from "react";
 import "./App.css";
 
 import Input from "./components/Input";
+import hookActions from "./actions/hookActions";
+
+// reducer to update state
+// @param state {object} - existing state, called automatically by  dispatch
+// @param action {object} - contains "type" and "payload" properties for the state update
+//                   for example: {type: "setSecretWord", payload: "party"}
+// @return {object} - new state
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "setSecretWord":
+      return { ...state, secretWord: action.payload };
+    default:
+      throw new Error(`invalid action type: ${action.type}`);
+  }
+}
 
 function App() {
+  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+
+  const setSecretWord = secretWord =>
+    dispatch({ type: "setSecretWord", payload: secretWord });
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord);
+  }, []);
+
   return (
     <div data-test="component-app">
       <h1>Jotto</h1>
--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,34 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import { findByTestAttr } from "./../test/testUtils";
 
+import hookActions from "./actions/hookActions";
 import App from "./App";
 
+const mockGetSecretWord = jest.fn();
+
 // Setup function for App component
-// @returns {ShallowWrapper}
+// @returns {MountWrapper}
 
 const setup = () => {
-  return shallow(<App />);
+  mockGetSecretWord.mockClear();
+  hookActions.getSecretWord = mockGetSecretWord;
+
+  // use mount, because useEffect is not called on `shallow`
+  return mount(<App />);
 };
 
 test("App renders without errors", () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, "component-app");
   expect(component.length).toBe(1);
+});
+
+describe("getSecretWord call", () => {
+  test("getSecretWord gets called on App mount", () => {
+    setup();
+
+    //check to see if secret word was updated
+    expect(mockGetSecretWord).toHaveBeenCalled();
+  });
 });
